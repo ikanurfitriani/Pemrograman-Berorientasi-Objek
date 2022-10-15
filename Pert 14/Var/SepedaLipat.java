dd58d457839d09f5fc760af030a3e56274016813
Ika Nurfitriani <90813508+ikanurfitriani@users.noreply.github.com>
2022-10-15 15:57:36 +0700

    Delete Pert 14 directory

--- Pert 14/Var/SepedaLipat.java
+++
@@ -1,7 +0,0 @@
-class SepedaLipat {
-  final String tipe = "United Black Horse";
-  public static void main(String args[]){
-    SepedaLipat sepedaIka = new SepedaLipat();
-    sepedaIka.tipe = "RoadBike Exotic 2708";
-  }
-}
